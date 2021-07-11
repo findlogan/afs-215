09ec5a37088f3da78d545fe2a2cb55c888ee3043
findlogan <logaster2@hotmail.com>
2021-07-10 21:12:44 -0400

    assignments

---
+++ week1/Fizzbuzzunittest.py
@@ -0,0 +1,7 @@
+def fizzbuzz(num):
+    if num == 1 :
+        print("Get", num)
+        return "1"
+    elif num == 2:
+        print("Get", num)
+        return "2"
---
+++ week1/function.py
@@ -0,0 +1,6 @@
+def perfectNum(num):
+    sum = 0
+    for i in range(1, num):
+        if num % i == 0:
+            sum += i
+    sum == num
